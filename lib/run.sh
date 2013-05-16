eac2b318aba3623aac19d85dd1b5eda2faafd963
Eugene Stepanov <evgeniy@goldenplanet.com>
2013-05-16 12:41:47 +0300

    configurations twig

--- lib/run.sh
+++ lib/run.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 SELENIUM="java -jar selenium-server-standalone.jar"
-
 if [ "$1" == "" ];
 then
 	echo "Usage:"
 	echo "run.sh hub|node"
+	exit
 fi
 
 if [ "$1" == "hub" ];
 then
-	$SELENIUM -role hub -hubConfig ../config/DefaultHub.json -log ../logs/hub.log > ../logs/selenium_hub_output.log 2>&1
+	$SELENIUM -role hub -hubConfig ../config/DefaultHub.json -log ../logs/hub.log > ../logs/selenium_hub_output.log 2>&1 & echo $! > ../logs/selenium.pid &
 fi
 
 if [ "$1" == "node" ];
 then
-	$SELENIUM -role node -nodeConfig ../config/DefaultLinuxDesktop.json -Dwebdriver.chrome.driver=chromedriver > ../logs/selenium_node_output.log 2>&1
+	$SELENIUM -role node -nodeConfig ../config/DefaultLinuxDesktop.json -Dwebdriver.chrome.driver=chromedriver > ../logs/selenium_node_output.log 2>&1 & echo $! > ../logs/selenium.pid &
 fi
-
